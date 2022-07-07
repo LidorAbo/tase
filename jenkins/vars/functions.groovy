c49a4a58cc06d6165fcfc0c81c4c129cbc056215
LidorAbo <lidorabo2@gmail.com>
2022-07-07 18:41:39 +0300

    Checking prepareEnv groovy function

--- jenkins/vars/functions.groovy
+++ jenkins/vars/functions.groovy
@@ -3,5 +3,11 @@ import groovy.json.JsonSlurper
 def prepareEnv(json) {
     def jsonSlurper = new JsonSlurper()
     def object = jsonSlurper.parseText(json)
-    echo "$object"
+    env.committer = object.head_commit.committer.name
+    env.email = object.head_commit.committer.email
+    env.latest_commit = object.head_commit.id
+    env.modified_files = object.head_commit.modified
+    env.removed_files = object.head_commit.removed
+    env.added_files = object.head_commit.added
+
 }
